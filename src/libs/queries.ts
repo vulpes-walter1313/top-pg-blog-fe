b31e7b754fbeea9913aeca67d52a9a80cad55e9e
Walter Fritsche <wfritsche95@protonmail.com>
2024-11-21 17:09:12 -0600

    added getPosts query

--- src/libs/queries.ts
+++ src/libs/queries.ts
@@ -1,3 +1,5 @@
+import { getErrorMessageFromReq } from "./handleErrors";
+
 export const getAuthCheck = async () => {
   const authToken = localStorage.getItem("auth_token");
   const fetchOptions: RequestInit = {
@@ -17,3 +19,26 @@ export const getAuthCheck = async () => {
   const data = await res.json();
   return data;
 };
+
+export const getPosts = async (page: number) => {
+  const authToken = localStorage.getItem("auth_token");
+  const fetchOptions: RequestInit = {
+    method: 'GET',
+    mode: "cors"
+  }
+  if (authToken) {
+    fetchOptions.headers = {
+     Authorization: authToken 
+    }
+  }
+  const res = await fetch(`http://localhost:3000/posts?limit=10&page=${page}&publishedstatus=published&summarize=true`, fetchOptions);
+  
+  if (!res.ok) {
+    const data = await res.json();
+    const message = getErrorMessageFromReq(data);
+    throw new Error(message || "Error in fetching posts");
+  }
+  const data = await res.json();
+  
+  return data;
+}
